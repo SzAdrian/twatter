1afcf95be037a9e94f3c42b7435fe4fa4c4976d4
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-20 15:10:19 +0200

    Create variables of Tweet model class

--- twatter-spring/src/main/java/com/codecool/twatterspring/model/Tweet.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/model/Tweet.java
@@ -9,5 +9,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+
 public class Tweet {
+
+    private Long id;
+
+    private Long userId;
+
+    private String content;
+
+    private LocalDateTime date;
+
 }
