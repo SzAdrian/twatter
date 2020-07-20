7f6b6e88aff13aa7013c45e494c6df7d12916078
botond <belgianmaniax@gmail.com>
2020-07-20 14:15:15 +0200

    Copy jwt functionality from previous projects

---
+++ twatter-spring/src/main/java/com/codecool/twatterspring/security/JwtService.java
@@ -0,0 +1,90 @@
+package com.codecool.twatterspring.security;
+
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+@Slf4j
+public class JwtService {
+
+//    @Value("${security.jwt.token.secret-key}")
+    private String secretKey = "secret";
+
+//    @Value("${security.jwt.token.expire-length:1800000}")
+    private final long validityInMilliSeconds = 1800000;
+
+    private final String rolesFieldName = "roles";
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username, List<String> roles) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(rolesFieldName, roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliSeconds);
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setIssuedAt(now)
+            .setExpiration(validity)
+            .signWith(SignatureAlgorithm.HS256, secretKey)
+            .compact();
+    }
+
+    String getTokenFromRequest(HttpServletRequest req) {
+        if (req.getCookies() == null) return null;
+        return Arrays.stream(req.getCookies())
+            .filter(cookie -> cookie.getName().equals("JWT"))
+            .findFirst().map(Cookie::getValue).orElse(null);
+    }
+
+    boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return claims.getBody().getExpiration().after(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            log.debug("JWT invalid " + e);
+        }
+        return false;
+    }
+
+    Authentication parseUserFromTokenInfo(String token) throws UsernameNotFoundException {
+        Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        var username = body.getSubject();
+        var roles = (List<String>) body.get(rolesFieldName);
+        var authorities = roles.stream()
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toCollection(LinkedList::new));
+        return new UsernamePasswordAuthenticationToken(username, "", authorities);
+    }
+
+    public void invalidateTokenCookie(HttpServletRequest req, HttpServletResponse res) {
+        Cookie tokenCookie = Arrays.stream(req.getCookies())
+            .filter(cookie -> "JWT".equals(cookie.getName())).findFirst()
+            .orElseThrow(() -> new AuthenticationCredentialsNotFoundException(
+                "Token cookie not present, invalid logout request.")
+            );
+        tokenCookie.setPath("/");
+        tokenCookie.setHttpOnly(true);
+        tokenCookie.setMaxAge(0);
+        res.addCookie(tokenCookie);
+    }
+}
--- twatter-spring/src/main/java/com/codecool/twatterspring/security/SecurityConfig.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/security/SecurityConfig.java
@@ -5,15 +5,36 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.cors()
+                .and()
+            .csrf()
+                .disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeRequests()
+                .antMatchers("/auth/*")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+                .and()
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
