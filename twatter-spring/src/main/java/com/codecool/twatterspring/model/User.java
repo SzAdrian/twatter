7bf13a8d97424a1eb04804c1dd1fee3b0dc4cf42
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-20 13:25:57 +0200

    Add fields to User entity

--- twatter-spring/src/main/java/com/codecool/twatterspring/model/User.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/model/User.java
@@ -6,10 +6,20 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class User {
+
+
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDateTime registrationDate;
+    private String password;
+
 }
