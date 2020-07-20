ed7f86e63d9be4d3369d1a52f40b5d44ab05f32f
botond <belgianmaniax@gmail.com>
2020-07-20 11:21:24 +0200

    Initialize Spring backend

---
+++ twatter-spring/src/main/java/com/codecool/twatterspring/TwatterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.twatterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwatterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwatterSpringApplication.class, args);
+	}
+
+}
---
+++ twatter-spring/src/test/java/com/codecool/twatterspring/TwatterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.twatterspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwatterSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
