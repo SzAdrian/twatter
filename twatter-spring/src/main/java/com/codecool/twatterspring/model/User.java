da7c083e7a03dbb394462a005a5f530ef6433527
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-20 13:20:37 +0200

    Add lombok annotations to User entity

---
+++ twatter-spring/src/main/java/com/codecool/twatterspring/model/User.java
@@ -0,0 +1,15 @@
+package com.codecool.twatterspring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+}
