e152b2cd39879717281fb4622558d0781a5b1370
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-20 13:30:27 +0200

    Add JPA annotations to User entity

--- twatter-spring/src/main/java/com/codecool/twatterspring/model/User.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/model/User.java
@@ -5,21 +5,31 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(name = "`user`")
 public class User {
 
-
+    @Id
+    @GeneratedValue
     private Long id;
+
+    @Column(unique = true)
     private String name;
+
+    @Column(unique = true)
     private String email;
+
+    @Column(nullable = false)
     private LocalDateTime registrationDate;
+
+    @Column(nullable = false)
     private String password;
 
 }
