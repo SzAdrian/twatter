5bb20624127f50be5d5b500606814c600efc2cdc
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-21 12:03:15 +0200

    Create empty method for TweetService

---
+++ twatter-spring/src/main/java/com/codecool/twatterspring/service/TweetService.java
@@ -0,0 +1,11 @@
+package com.codecool.twatterspring.service;
+
+import com.codecool.twatterspring.model.TimelineTweetDTO;
+
+import java.util.List;
+
+public class TweetService {
+    public List<TimelineTweetDTO> getAllByUserId(Long userId) {
+        return null;
+    }
+}
