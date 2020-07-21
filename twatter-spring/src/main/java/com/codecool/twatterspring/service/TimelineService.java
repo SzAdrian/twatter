3aa884a3a75d563d0076e6e70d19d32424f2e68c
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-21 12:05:07 +0200

    Reference TweetService from TimelineService

--- twatter-spring/src/main/java/com/codecool/twatterspring/service/TimelineService.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/service/TimelineService.java
@@ -1,4 +1,12 @@
 package com.codecool.twatterspring.service;
 
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
 public class TimelineService {
+
+    private final TweetService tweetService;
+
+
 }
