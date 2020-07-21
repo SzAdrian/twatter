b6dd50e28bba836681bfdd35ec65140c8d53ff28
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-21 12:08:08 +0200

    Reference TimelineService from TimelineController

--- twatter-spring/src/main/java/com/codecool/twatterspring/controller/TimelineController.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/controller/TimelineController.java
@@ -1,6 +1,8 @@
 package com.codecool.twatterspring.controller;
 
 import com.codecool.twatterspring.model.TimelineTweetDTO;
+import com.codecool.twatterspring.service.TimelineService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class TimelineController {
+
+    private final TimelineService timelineService;
 
     @GetMapping("/users/{id}/user-timeline")
     public List<TimelineTweetDTO> userTimeline(@PathVariable("id") Long user_id){
         return new ArrayList<>();
     }
+
+
 }
