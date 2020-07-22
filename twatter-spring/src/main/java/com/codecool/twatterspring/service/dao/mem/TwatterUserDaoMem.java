1e57ad7accd1e251c6a62bb56ef5ff429b571a7f
botond <belgianmaniax@gmail.com>
2020-07-22 12:57:05 +0200

    Configure profiles

--- twatter-spring/src/main/java/com/codecool/twatterspring/service/dao/mem/TwatterUserDaoMem.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/service/dao/mem/TwatterUserDaoMem.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@Profile("development")
+@Profile("development & no_db")
 @Qualifier("twatterUserDao")
 public class TwatterUserDaoMem implements TwatterUserDao {
 
