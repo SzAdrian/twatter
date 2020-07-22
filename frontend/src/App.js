cd6663ba20b64cbbd2b32e58da0d5a79baf9bcbf
SzAdrian <51090249+SzAdrian@users.noreply.github.com>
2020-07-22 16:43:11 +0200

    login route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
 import GlobalStyle from "./components/style-components/global/GlobalStyle";
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from "./components/main-page/MainPage";
+import LoginPage from "./components/login/LoginPage";
 
 function App() {
-    return (
-        <div className="App">
-            <GlobalStyle />
+  return (
+    <div className="App">
+      <GlobalStyle />
 
-                <Router>
-                    <Route exact path="/" component={MainPage} />
-                </Router>
-
-        </div>
-    );
+      <Router>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/login" component={LoginPage} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
