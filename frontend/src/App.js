3ef8f0e2e77febaca3d01cb332cc61dcebbe3e13
Adrián Szentkuti <51090249+SzAdrian@users.noreply.github.com>
2020-07-23 16:17:03 +0200

    signup route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import GlobalStyle from "./components/style-components/global/GlobalStyle";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainPage from "./components/main-page/MainPage";
 import LoginPage from "./components/login/LoginPage";
+import SignUp from "components/Signup/SignUpPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Route exact path="/" component={MainPage} />
         <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/signup" component={SignUp} />
       </Router>
     </div>
   );
