134756cdaf77e8f88cb18883519518c5a5aee4ba
JenesesD <jenesesd@gmail.com>
2020-07-23 10:44:14 +0200

    LeftSideBar component

--- frontend/src/components/main-page/Left-side-bar/LeftSideBar.js
+++ frontend/src/components/main-page/Left-side-bar/LeftSideBar.js
@@ -1,23 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 
-const LeftSideBarStyle = styled.div`
-    
-    grid-area: leftBar;
-    background-color: red;
-    
-  
-`
+import LeftUpperPanel from "./LeftUpperPanel";
+import LeftBottomPanel from "./LeftBottomPanel";
 
+const LeftSideBarStyle = styled.div`
+  grid-area: leftBar;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 function LeftSideBar() {
-
-    return (
-        <LeftSideBarStyle>
-
-        </LeftSideBarStyle>
-    )
-
+  return (
+    <LeftSideBarStyle>
+      <LeftUpperPanel />
+      <LeftBottomPanel />
+    </LeftSideBarStyle>
+  );
 }
 
 export default LeftSideBar;
