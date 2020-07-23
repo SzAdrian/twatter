9a8216855266a749f53a8d918423829b06f5b8d1
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-07-23 12:48:13 +0200

    Add timelinetweet dto to handle outgoing timeline tweet data

---
+++ twatter-spring/src/main/java/com/codecool/twatterspring/model/dto/TimelineTweetDTO.java
@@ -0,0 +1,18 @@
+package com.codecool.twatterspring.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TimelineTweetDTO {
+    private Long id;
+    private String content;
+    private Long userId;
+    private String userName;
+    private String postedAt;
+}
