3bf92733dd8584fa935d56846d696b7ebd4fe36f
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-07-23 13:52:18 +0200

    Add method to twatter user repository to return the followees only of the given user

--- twatter-spring/src/main/java/com/codecool/twatterspring/repository/TwatterUserRepository.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/repository/TwatterUserRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface TwatterUserRepository extends JpaRepository<TwatterUser, Long> {
 
     @Query("SELECT u.name FROM TwatterUser u WHERE u.id = :userId")
-
     String getUsernameByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT u.followees FROM TwatterUser u WHERE u.id = :userId")
+    List<TwatterUser> getFolloweesByUserId(@Param("userId") Long userId);
 }
