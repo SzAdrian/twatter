f4f28f252ad6250560588ff0ff606c7740e18967
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-07-29 10:57:12 +0200

    Add getUserProfile to UserService

--- twatter-spring/src/main/java/com/codecool/twatterspring/service/UserService.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/service/UserService.java
@@ -1,12 +1,21 @@
 package com.codecool.twatterspring.service;
 
+import com.codecool.twatterspring.model.dto.TwatterUserProfileDTO;
 import com.codecool.twatterspring.repository.TwatterUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final TwatterUserRepository users;
+
+    public TwatterUserProfileDTO getUserProfileBy(Long userId) {
+        return users.findById(userId)
+                .map(TwatterUserProfileDTO::fromEntity)
+                .orElseThrow(() -> new EntityNotFoundException("The given userId does not exist!"));
+    }
 }
