f7d604ff68d313b4e4203a4c127dfff949a265f2
botond <belgianmaniax@gmail.com>
2020-07-29 09:57:53 +0200

    Rename endpoints

--- twatter-spring/src/main/java/com/codecool/twatterspring/controller/TimelineController.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/controller/TimelineController.java
@@ -5,22 +5,24 @@ import com.codecool.twatterspring.service.TimelineService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/timeline")
 public class TimelineController {
 
     private final TimelineService timelineService;
 
-    @GetMapping("/users/{id}/user-timeline")
+    @GetMapping("/user/{id}")
     public List<TimelineTweetDTO> userTimeline(@PathVariable("id") Long userId){
         return this.timelineService.getUserTimeline(userId);
     }
 
-    @GetMapping("/tweets/{id}/home-timeline")
+    @GetMapping("/home/{id}")
     public List<TimelineTweetDTO> homeTimeline(@PathVariable("id") Long userId){
         return this.timelineService.getHomeTimeline(userId);
     }
--- twatter-spring/src/main/java/com/codecool/twatterspring/controller/TweetController.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/controller/TweetController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tweets")
+@RequestMapping("/api/tweets")
 @RequiredArgsConstructor
 public class TweetController {
 
