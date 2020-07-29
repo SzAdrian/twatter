952fe58950729507d24327bb6e70d50e1b4b0a81
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-07-29 18:21:18 +0200

    Add TrendingHashtagsDTO to handle trending hashtags

---
+++ twatter-spring/src/main/java/com/codecool/twatterspring/model/dto/TrendingHashtagsDTO.java
@@ -0,0 +1,16 @@
+package com.codecool.twatterspring.model.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonSerialize
+public class TrendingHashtagsDTO {
+    private String[] trendingHashtags;
+}
