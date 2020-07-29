ab255a0bdb298f12b932c366108eb47287500923
Rajnai Máté <ramate.rajnai@gmail.com>
2020-07-29 14:33:41 +0200

    Test follow user

--- twatter-spring/src/test/java/com/codecool/twatterspring/service/UserServiceIntegrationTest.java
+++ twatter-spring/src/test/java/com/codecool/twatterspring/service/UserServiceIntegrationTest.java
@@ -1,19 +1,27 @@
 package com.codecool.twatterspring.service;
 
 import com.codecool.twatterspring.model.TwatterUser;
+import com.codecool.twatterspring.redis.TimelineRepository;
 import com.codecool.twatterspring.repository.TwatterUserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
 @ActiveProfiles("test")
+@Import(UserService.class)
 class UserServiceIntegrationTest {
 
     @Autowired
@@ -21,6 +29,9 @@ class UserServiceIntegrationTest {
 
     @Autowired
     private UserService userService;
+
+    @MockBean
+    private TimelineRepository timelineRepository;
 
     @BeforeEach
     public void init() {
@@ -47,7 +58,7 @@ class UserServiceIntegrationTest {
                         .password("gergo")
                         .registrationDate(LocalDateTime.now())
                         .email("gergo@kovacs.com")
-                        .followees(List.of(user1, user2))
+                        .followees(new ArrayList<>(Arrays.asList(user1, user2)))
                         .build()
         );
 
@@ -57,10 +68,16 @@ class UserServiceIntegrationTest {
                         .password("teszt")
                         .registrationDate(LocalDateTime.now())
                         .email("teszt@elek.com")
-                        .followees(List.of(user1, user3))
+                        .followees(new ArrayList<>(Arrays.asList(user1, user3)))
                         .build()
         );
 
+    }
+
+    @Test
+    public void testFollowUser() {
+        userService.followUser(3L, 4L);
+        assertThat(users.getFolloweesByUserId(3L)).hasSize(3);
     }
 
 
