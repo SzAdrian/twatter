8d5b08c3f8cdf08b6106d8c32dd638ac7d4b26d5
Gergo Kovacs <kovacsgergo128@gmail.com>
2020-07-29 11:17:16 +0200

    Add followUser method to UserService

--- twatter-spring/src/main/java/com/codecool/twatterspring/service/UserService.java
+++ twatter-spring/src/main/java/com/codecool/twatterspring/service/UserService.java
@@ -1,11 +1,14 @@
 package com.codecool.twatterspring.service;
 
+import com.codecool.twatterspring.model.TwatterUser;
 import com.codecool.twatterspring.model.dto.TwatterUserProfileDTO;
 import com.codecool.twatterspring.repository.TwatterUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -17,5 +20,18 @@ public class UserService {
         return users.findById(userId)
                 .map(TwatterUserProfileDTO::fromEntity)
                 .orElseThrow(() -> new EntityNotFoundException("The given userId does not exist!"));
+    }
+    @Transactional
+    public void followUser(Long userId, Long followeeId) {
+        TwatterUser user = users.findById(userId)
+                                .orElseThrow(() -> new EntityNotFoundException("The given userId does not exist!"));
+
+        TwatterUser followee = users.findById(followeeId)
+                                .orElseThrow(() -> new EntityNotFoundException("The given followeeId does not exist!"));
+
+        List<TwatterUser> followees = user.getFollowees();
+        followees.add((followee));
+
+        users.save(user);
     }
 }
