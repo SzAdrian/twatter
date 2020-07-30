3c487c5a2f19a4a6553e31a6217b2f0c3ca513c4
KlaraHajdu <49884249+KlaraHajdu@users.noreply.github.com>
2020-07-30 17:02:43 +0200

    Delete unnecessary attribute

--- C#Backend/Twatter-Backend-csharp/Models/Hashtag.cs
+++ C#Backend/Twatter-Backend-csharp/Models/Hashtag.cs
@@ -9,7 +9,6 @@ namespace Twatter_Backend_csharp.Models
 
         public int Id { get; set; }
         [Required]
-        [RegularExpression(@"^\w+$", ErrorMessage = "Hashtag cannot contain special or break characters.")]
         public string Name { get; set; }
         
         [DataType(DataType.DateTime)]
