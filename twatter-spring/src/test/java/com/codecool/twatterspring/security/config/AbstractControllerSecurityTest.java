88d8ab2dc6a6b0b17fdb1c03c3bc082cf9a9a345
botond <belgianmaniax@gmail.com>
2020-07-31 11:35:48 +0200

    Create AbstractControllerSecurityTest and concrete implementation for AuthController

--- twatter-spring/src/test/java/com/codecool/twatterspring/security/config/AbstractControllerSecurityTest.java
+++ twatter-spring/src/test/java/com/codecool/twatterspring/security/config/AbstractControllerSecurityTest.java
@@ -4,36 +4,28 @@ import com.codecool.twatterspring.redis.TimelineRepository;
 import com.codecool.twatterspring.security.JwtFilter;
 import com.codecool.twatterspring.security.service.AuthService;
 import com.codecool.twatterspring.security.service.JwtService;
+import com.codecool.twatterspring.security.service.TwatterUserDetailsService;
 import com.codecool.twatterspring.service.TimelineService;
 import com.codecool.twatterspring.service.TweetService;
-import com.codecool.twatterspring.service.dao.TwatterUserDao;
-import com.codecool.twatterspring.service.dao.TwatterUserDaoDb;
 import com.codecool.twatterspring.service.dao.mem.TwatterUserDaoMem;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@WebMvcTest
 @ActiveProfiles({"test", "security", "no_db"})
-@Import({AuthService.class, JwtService.class, TwatterUserDaoMem.class, JwtFilter.class})
-class ProductionSecurityConfigTest {
+@Import({AuthService.class, JwtService.class, TwatterUserDaoMem.class, JwtFilter.class, TwatterUserDetailsService.class})
+abstract class AbstractControllerSecurityTest {
+
+    static final String BASE_URL = "/api";
+    static final ObjectMapper mapper = new ObjectMapper();
 
     @MockBean
-    private TimelineRepository timelineRepository;
-    
+    TimelineRepository timelineRepository;
+
     @MockBean
-    private TimelineService timelineService;
-    
+    TimelineService timelineService;
+
     @MockBean
-    private TweetService tweetService;
-    
-    @Test
-    void smokeTest() {
-    }
+    TweetService tweetService;
 }
