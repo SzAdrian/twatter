38ee89b5267ea132c982c6c55124824232fb206f
Adrián Szentkuti <51090249+SzAdrian@users.noreply.github.com>
2020-08-06 10:56:09 +0200

    private routes created

--- frontend/src/components/hooks/useTheme.js
+++ frontend/src/components/hooks/useTheme.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import storage from "local-storage-fallback";
 
 export default function useTheme(
-  defaultTheme = { mode: "light", secondary: "blue", textSize: "normal" }
+  defaultTheme = { mode: "dim", secondary: "blue", textSize: "normal" }
 ) {
   const [theme, _setTheme] = useState(getInitialTheme);
 
